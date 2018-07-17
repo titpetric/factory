24c54d6f22919d7cacc584d1ed379e4ed3afb4e9
Tit Petric <black@scene-si.org>
2018-07-17 19:40:28 +0200

    add database tests, add update query, add profiling to namedexec

--- database_test.go
+++ database_test.go
@@ -5,17 +5,61 @@ import (
 )
 
 func TestDatabase(t *testing.T) {
+	assert := func(result bool, format string, params ...interface{}) {
+		if !result {
+			t.Errorf(format, params...)
+		}
+	}
+
 	db := DB{}
-	if db.DB != nil {
-		t.Errorf("DB instance expected nil")
-	}
-	if db.Profiler != nil {
-		t.Errorf("DB profiler expected nil")
-	}
+	assert(db.DB == nil, "DB instance expected nil")
+	assert(db.Profiler == nil, "DB profiler expected nil")
 
 	db.Profiler = &DatabaseProfilerStdout{}
 
-	if db.Quiet().Profiler != nil {
-		t.Errorf("DB quiet profiler expected nil")
+	assert(db.Quiet().Profiler == nil, "DB quiet profiler expected nil")
+
+	dbStruct := struct {
+		ID    int    `db:"id"`
+		Name  string `db:"name,omitempty"`
+		Title string `db:"-"`
+	}{123, "Tit Petric", "Sir"}
+
+	set := db.setMap(&dbStruct)
+
+	if len(set) != 2 {
+		t.Errorf("Expected setmap length 2, got %d", len(set))
+	}
+
+	// check expected contents of set
+	{
+		check := func(key string) {
+			val, ok := set[key]
+			assert(ok, "Expected existance of set[%s]", key)
+			if ok {
+				assert(val == ":"+key, "Expected value of set[%s] = :%s, got %s", key, key, val)
+			}
+		}
+		check("id")
+		check("name")
+	}
+
+	// check set functionality
+	{
+		i := db.set(&dbStruct)
+		assert(i == "id=:id, name=:name" || i == "name=:name, id=:id", "Unexpected set() result: %s", i)
+	}
+	{
+		i := db.setImplode(", ", set)
+		assert(i == "id=:id, name=:name" || i == "name=:name, id=:id", "Unexpected setImplode() 1 result: %s", i)
+	}
+	{
+		i := db.setImplode(" AND ", set)
+		assert(i == "id=:id AND name=:name" || i == "name=:name AND id=:id", "Unexpected setImplode() 2 result: %s", i)
+	}
+	{
+		delete(set, "name")
+		i := db.setImplode(" AND ", set)
+		assert(i == "id=:id", "Unexpected setImplode() 3 result: %s", i)
 	}
 }
